0e401ac5f4b77a8705cb3843290a7da78925da60
SunWoo <sunwoo3856@naver.com>
2024-05-07 21:14:24 +0900

    first commit

---
+++ src/test/java/com/coders/diaryservice/DiaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coders.diaryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
