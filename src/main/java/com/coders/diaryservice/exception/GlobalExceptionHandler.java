7408807f3326a1f5f446d7a2636fce8acac613f7
SunWoo <sunwoo3856@naver.com>
2024-07-21 18:22:03 +0900

    코드 최적화 및 수정

--- src/main/java/com/coders/diaryservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/coders/diaryservice/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.coders.diaryservice.exception;
 
 import com.coders.diaryservice.exception.eventTag.DuplicateEventTagUpdateException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
@@ -10,14 +11,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.net.URI;
+import java.util.Arrays;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ProblemDetail> allExceptionHandle(Exception ex, WebRequest request) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
         problemDetail.setTitle("예상치 못한 오류");
+        log.error(Arrays.toString(ex.getStackTrace()));
 
         return new ResponseEntity<>(problemDetail, HttpStatus.INTERNAL_SERVER_ERROR);
     }
